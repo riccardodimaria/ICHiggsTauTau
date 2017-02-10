4b874bbb8a17bd9dece120aec2081c23e9dfcfdc
Riccardo Di Maria <r.di-maria15@imperial.ac.uk>
2017-02-10 10:19:32 +0000

    MC LTs

--- Analysis/HiggsNuNu/scripts/submit_lighttree_mcRDM.sh
+++ Analysis/HiggsNuNu/scripts/submit_lighttree_mcRDM.sh
@@ -31,20 +31,20 @@ INPUTPARAMS="filelists/$PRODUCTION/Params${PRODUCTION}.dat"
 CONFIG=scripts/DefaultLightTreeConfig_mc.cfg
 
 
-for SYST in central #JESUP JESDOWN JERBETTER JERWORSE UESUP UESDOWN ELEEFFUP ELEEFFDOWN MUEFFUP MUEFFDOWN #NOTE TO RUN JER DOSMEAR MUST BE SET TO TRUE IN THE CONFIG
+#for SYST in central #JESUP JESDOWN JERBETTER JERWORSE UESUP UESDOWN ELEEFFUP ELEEFFDOWN MUEFFUP MUEFFDOWN #NOTE TO RUN JER DOSMEAR MUST BE SET TO TRUE IN THE CONFIG
 #for SYST in JESUP JESDOWN JERBETTER
 #for SYST in JERWORSE UESUP UESDOWN
 #for SYST in ELEEFFUP ELEEFFDOWN
-#for SYST in MUEFFUP MUEFFDOWN
+for SYST in MUEFFUP MUEFFDOWN
   do
   SYSTOPTIONS="--dojessyst=false --dojersyst=false"
 
   #JOBDIRPREFIX=/vols/cms/rd1715/HiggsToInv/jobs_lighttree_${PRODUCTION}_ICHEP
-  JOBDIRPREFIX=/vols/cms/rd1715/HiggsToInv/jobs_lighttree_170209_ReReco
+  JOBDIRPREFIX=/vols/cms/rd1715/HiggsToInv/jobs_lighttree_170211_ReReco
   #JOBDIRPREFIX=/vols/cms/magnan/Hinvisible/RunIILT/jobs_lighttree_170111
   JOBDIR=$JOBDIRPREFIX/
   #OUTPUTPREFIX=/vols/cms/rd1715/HiggsToInv/output_lighttree_${PRODUCTION}_ICHEP
-  OUTPUTPREFIX=/vols/cms/rd1715/HiggsToInv/output_lighttree_170209_ReReco
+  OUTPUTPREFIX=/vols/cms/rd1715/HiggsToInv/output_lighttree_170211_ReReco
   #OUTPUTPREFIX=/vols/cms/magnan/Hinvisible/RunIILT/output_lighttree_170111
   OUTPUTDIR=$OUTPUTPREFIX/
 
@@ -150,6 +150,7 @@ for SYST in central #JESUP JESDOWN JERBETTER JERWORSE UESUP UESDOWN ELEEFFUP ELE
     #for FILELIST in `ls filelists/$PRODUCTION/$QUEUEDIR/*_MC_EWK*`
     #for FILELIST in `ls filelists/$PRODUCTION/$QUEUEDIR/*_MC_*NuNu*`
     for FILELIST in `ls filelists/$PRODUCTION/$QUEUEDIR/*_MC_*`
+    #for FILELIST in `ls filelists/$PRODUCTION/$QUEUEDIR/*_MC_TT-powheg_split59*`
       do
       echo "Processing files in "$FILELIST
 
